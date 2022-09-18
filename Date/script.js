c90297acb74665678c3d119f4b04e2872cf73c1a
Aliaksandr Mamchyts <al.mamchyts@gmail.com>
2022-09-18 21:46:47 +0300

    feat: complite test JS Basics

--- Date/script.js
+++ Date/script.js
@@ -1,13 +1,33 @@
-function formatDuration (seconds) {
-    let result = '';
-    let hours = null;
+// function formatDuration (seconds) {
+//     let result = '';
+//     let hours = null;
 
-    let getHour = (seconds) => {
-        let hour = seconds / 3600;
-        return hour;
-    }
+//     let getHour = (seconds) => {
+//         let hour = seconds / 3600;
+//         return hour;
+//     }
     
-    console.log('hour --- ', getHour(seconds));
-  }
+//     console.log('hour --- ', getHour(seconds));
+//   }
 
-console.log('result ----- ', formatDuration(4662));
+// console.log('result ----- ', formatDuration(4662));
+
+
+const name = 'Vasia';
+
+let a = true;
+let b = true;
+/*let b = 86;*/
+
+let max = (8/2 + 5- - 3/2)  
+
+console.log("it/'s cool -- ", typeof '');
+
+let i = 10; 
+
+while (i < 10) {
+    console.log(' ii ');
+}
+
+
+
