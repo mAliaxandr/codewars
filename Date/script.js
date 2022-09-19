7279981cf91ab0762a2511512414e4d3edca4e7f
Aliaksandr Mamchyts <al.mamchyts@gmail.com>
2022-09-19 22:59:46 +0300

    init: start test JS Types Test

--- Date/script.js
+++ Date/script.js
@@ -13,21 +13,6 @@
 // console.log('result ----- ', formatDuration(4662));
 
 
-const name = 'Vasia';
-
-let a = true;
-let b = true;
-/*let b = 86;*/
-
-let max = (8/2 + 5- - 3/2)  
-
-console.log("it/'s cool -- ", typeof '');
-
-let i = 10; 
-
-while (i < 10) {
-    console.log(' ii ');
-}
 
 
 
