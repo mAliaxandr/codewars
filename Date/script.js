d9fd4722185876bc14c7241622e5d813f431c1ed
Aliaksandr Mamchyts <al.mamchyts@gmail.com>
2022-09-20 23:43:17 +0300

    feat: complite test JS Tapes

--- Date/script.js
+++ Date/script.js
@@ -12,7 +12,14 @@
 
 // console.log('result ----- ', formatDuration(4662));
 
+let v = (!!'0');
+console.log('-res-- ',   0 && '' && []);
 
+if ({}=={}) {
+    console.log(true);
+} else {
+    console.log(false);
+}
 
 
 
